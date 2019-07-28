c1cab3d99d8a7c31a71e4a9ad5faa662f36fe90c
Leo Li <fs100723@gamil.com>
2019-07-28 22:55:02 +0800

    project init part 2

---
+++ src/components/home/home.js
@@ -0,0 +1,15 @@
+const data = {
+  msg: 'hello Vue.js!'
+}
+const changeTitle = (title) => {
+  data.msg = title
+}
+export default {
+  name: 'Home',
+  data () {
+    return data
+  },
+  methods: {
+    changeTitle
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,27 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    count: 10
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  },
+  strict: process.env.NODE_ENV !== 'production'
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
+  store
 })
